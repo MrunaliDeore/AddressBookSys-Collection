42f19e1daba5971db55855f74f66f5c5fb062bd0
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-02 12:46:00 +0530

    Added : main class file - msg

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package Collection;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Assignment No. 22\n");
+		System.out.println("Using Collection Library to solve the \nAddress Book Problem from UC7 to UC 11");
+	}
+}

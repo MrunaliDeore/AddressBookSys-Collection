2f6e3630aaab72325212bbbebdfb02464c5a68ab
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-05 10:40:02 +0530

    Added : user registrstion code by using collection

---
+++ AddressBook.java
@@ -0,0 +1,18 @@
+package Collection;
+import java.util.*;
+import java.lang.*;
+
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+}
